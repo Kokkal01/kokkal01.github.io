c7a28b6f8c6084d723f417d5b93d18f06bdd0b38
Kokkal01 <Kokkal01@luther.edu>
2015-10-16 13:10:40 -0500

    Update spa.js

--- spa.js
+++ spa.js
@@ -1,6 +1,11 @@
 ex1 = function() {
   var Task;
   myInput = document.querySelector('#Task').value;
-  Task = document.querySelector('#New')
-  Task.innerHTML = myInput;
+  list = document.createElement("ol");
+  item = document.createElement("li");
+  ol = document.createTextNode();
+  li = document.createTextNode(text);
+  list.appendChild(ol)
+  item.appendChild(li)
+
 }
