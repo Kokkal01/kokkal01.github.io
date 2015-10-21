2c9579a6c69e34ee8e93bb092a6b42f5c5da3db8
Kokkal01 <Kokkal01@luther.edu>
2015-10-21 12:11:37 -0500

    Update spa.js

--- spa.js
+++ spa.js
@@ -1,5 +1,18 @@
 ex1 = function() {
   var myList;
+  myBox = document.querySelector(“#textbox”);
+  myPriority = document.querySelector(“#priority”);
+  myUl = document.querySelector(“#tasklist”);
+  myUl.appendChild(myLi);
+  myLi = document.createElement(“li”);
+  myLi.classList.add(myPriority.value);
+  myLi.appendChild(myInput);
+  myLi.appendChild(myText);
+  myInput = document.createElement(“INPUT”);
+  myInput.type = “checkbox”;
+  myText = document.createTextNode(myBox.value);
+  
+  
   myList = document.querySelector('#nodot');
   nodot = document.createElement("li");
   li = document.createTextNode("input");
